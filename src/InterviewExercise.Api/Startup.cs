f50753d5af38c60904fbc24ed8d6c1236049bf58
Kseinya Reyes <kseinyar@stcu.org>
2018-08-21 17:05:13 -0700

    Updating seed data

--- src/InterviewExercise.Api/Startup.cs
+++ src/InterviewExercise.Api/Startup.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace InterviewExercise.Api
 {
@@ -25,6 +18,9 @@ namespace InterviewExercise.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //DefaultTypeMap.MatchNamesWithUnderscores = true;
+            //var sqlConnection = "Data Source=(LOCALDB)\\PROJECTSV13;Initial Catalog = Database.InterviewExercise";
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -40,7 +36,7 @@ namespace InterviewExercise.Api
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
