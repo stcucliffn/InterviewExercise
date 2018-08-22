968350515c6ed728418f8e23557853d636eda110
Aaron Nay <aaronn@stcu.org>
2018-08-22 08:34:33 -0700

    WIP towards final version of client without backend.

--- src/interview-exercise-client/src/app/app-routing.module.ts
+++ src/interview-exercise-client/src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AccountListComponent } from './components/account-list/account-list.component';
 import { ErrorComponent } from './components/error/error.component';
+import { AccountDetailComponent } from './components/account-detail/account-detail.component';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'accounts',
     pathMatch: 'full'
+  },
+  {
+    path: 'accounts/detail/:id',
+    component: AccountDetailComponent
   },
   {
     path: 'accounts',
--- src/interview-exercise-client/src/app/components/account-detail/account-detail.component.ts
+++ src/interview-exercise-client/src/app/components/account-detail/account-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { AccountsService } from '../../core/services/accounts.service';
+import { Account } from '../../core/models/account';
 
 @Component({
   selector: 'app-account-detail',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-detail.component.scss']
 })
 export class AccountDetailComponent implements OnInit {
+  public account: Account;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private accountsService: AccountsService
+  ) {}
 
   ngOnInit() {
+    this.accountsService
+      .getAccount(this.route.snapshot.paramMap.get('id'), 1)
+      .subscribe((account: Account) => {
+        this.account = account;
+      });
   }
-
 }
